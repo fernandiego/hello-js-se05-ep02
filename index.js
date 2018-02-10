708355258eb39219716e406b7676cbb587ba32c2
Fox <fernandodiego.sn@gmail.com>
2018-02-10 17:12:07 -0300

    aprendi

---
+++ index.js
@@ -0,0 +1,25 @@
+const a = process.argv[2]
+
+function parImpar(x) {
+    if (x % 2 == 0) {
+        console.log("par")
+
+    }
+    else {
+        console.log("impar")
+    }
+}
+
+parImpar(a)
+
+function div4(x) {
+    if (x % 4 == 0) {
+        console.log("É divisiadfiadfsfgpjsdf")
+
+    }
+    else {
+        console.log("FODA SE")
+    }
+}
+
+div4(a)
